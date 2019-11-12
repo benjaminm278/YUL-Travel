59aae74277440944665855889b9f640f4ceacbc7
benjaminm278 <mbenjamin0279@gmail.com>
2019-11-12 13:29:32 -0500

    Add activities

---
+++ app/src/main/java/com/example/yultravel/SpotsActivity.java
@@ -0,0 +1,13 @@
+package com.example.yultravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpotsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spots);
+    }
+}
