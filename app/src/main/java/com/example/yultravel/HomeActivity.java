76019e1337d50f932fff79c6cc04559bbed22e0e
benjaminm278 <mbenjamin0279@gmail.com>
2019-11-13 09:55:52 -0500

    Link image buttons to activities in home screen

--- app/src/main/java/com/example/yultravel/HomeActivity.java
+++ app/src/main/java/com/example/yultravel/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.yultravel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,36 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    /**
+     * Opens an activity based on the image button that was clicked
+     * @param view
+     */
+    public void openActivity(View view) {
+        //Toast.makeText(this, view.getId() + " ", Toast.LENGTH_SHORT).show();
+
+        // Variables
+        Intent activityIntent;
+
+        switch (view.getId()) {
+            case R.id.weatherImageButton:
+                activityIntent = new Intent(this, WeatherActivity.class);
+                break;
+            case R.id.plansImageButton:
+                activityIntent = new Intent(this, PlansActivity.class);
+                break;
+            case R.id.spotsImageButton:
+                activityIntent = new Intent(this, SpotsActivity.class);
+                break;
+            case R.id.directionsImageButton:
+                activityIntent = new Intent(this, DirectionsActivity.class);
+                break;
+            default:
+                activityIntent = new Intent();
+                break;
+        }
+
+        startActivity(activityIntent);
     }
 }
