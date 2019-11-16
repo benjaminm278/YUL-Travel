4285ca38b2a162aa8b86b882c4ec9ddf936985bf
benjaminm278 <mbenjamin0279@gmail.com>
2019-11-16 05:29:08 +0100

    Directions activity

--- app/src/main/java/com/example/yultravel/DirectionsActivity.java
+++ app/src/main/java/com/example/yultravel/DirectionsActivity.java
@@ -2,6 +2,10 @@ package com.example.yultravel;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class DirectionsActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class DirectionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
+
+        // String arraylist
+        ArrayList<String> categoryNames = new ArrayList();
+        categoryNames.add("Airport");
+        categoryNames.add("Restaurants");
+        categoryNames.add("Metro (subway)");
+        categoryNames.add("Parks");
+        categoryNames.add("Shops");
+        categoryNames.add("Stadium");
+
+        // RecyclerView code
+        RecyclerView directionsRC = findViewById(R.id.directionsCategoryRecyclerView);
+        DirectionsCategoryAdapter adapter = new DirectionsCategoryAdapter(this, categoryNames);
+        directionsRC.setAdapter(adapter);
+        directionsRC.setLayoutManager(new LinearLayoutManager(this));
     }
 }
