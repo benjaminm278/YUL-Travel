2b8715a58ded0b7bdd7e9a259707d2f70069e064
benjaminm278 <mbenjamin0279@gmail.com>
2019-11-16 18:12:23 +0100

    Directions activity

--- app/src/main/java/com/example/yultravel/DirectionsCategoryAdapter.java
+++ app/src/main/java/com/example/yultravel/DirectionsCategoryAdapter.java
@@ -18,13 +18,23 @@ import java.util.ArrayList;
 public class DirectionsCategoryAdapter extends RecyclerView.Adapter<DirectionsCategoryAdapter.DirectionsCategoryViewHolder> {
     private ArrayList<String> category_names;
     private LayoutInflater mInflater;
+    private Context ctx;
 
-    public static class DirectionsCategoryViewHolder extends RecyclerView.ViewHolder {
+    public static class DirectionsCategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final TextView categoryTxt;
-        public DirectionsCategoryViewHolder(@NonNull View itemView) {
+        DirectionsCategoryAdapter adapter;
+        public DirectionsCategoryViewHolder(@NonNull View itemView, DirectionsCategoryAdapter adapter) {
             super(itemView);
 
+            this.adapter = adapter;
             categoryTxt = itemView.findViewById(R.id.categoryTextView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            adapter.openGoogleMaps(categoryTxt.getText().toString());
         }
     }
 
@@ -36,6 +46,16 @@ public class DirectionsCategoryAdapter extends RecyclerView.Adapter<DirectionsCa
     public DirectionsCategoryAdapter(Context ctx, ArrayList<String> category_names) {
         mInflater = LayoutInflater.from(ctx);
         this.category_names = category_names;
+        this.ctx = ctx;
+    }
+
+    /**
+     * Opens google maps (soon to be implemented)
+     * @param category
+     */
+    public void openGoogleMaps(String category) {
+        // Test code to verify that on click listener works as intended
+        Toast.makeText(ctx, "Category clicked: " + category, Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -48,11 +68,11 @@ public class DirectionsCategoryAdapter extends RecyclerView.Adapter<DirectionsCa
     @Override
     public DirectionsCategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView = mInflater.inflate(R.layout.category_cardview, parent,false);
-        return new DirectionsCategoryAdapter.DirectionsCategoryViewHolder(mItemView);
+        return new DirectionsCategoryAdapter.DirectionsCategoryViewHolder(mItemView, this);
     }
 
     /**
-     *
+     * Sets the text of a textview at a given position
      * @param holder
      * @param position
      */
