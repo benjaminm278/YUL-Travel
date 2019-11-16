de522423de16757a8c7181af1065a40c223e0605
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2019-11-15 21:43:26 -0500

    Fully implement RecyclerView adapter class

--- app/src/main/java/com/example/yultravel/DirectionsActivity.java
+++ app/src/main/java/com/example/yultravel/DirectionsActivity.java
@@ -2,7 +2,10 @@ package com.example.yultravel;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class DirectionsActivity extends AppCompatActivity {
 
@@ -11,8 +14,15 @@ public class DirectionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
 
+        // String arraylist
+        ArrayList<String> categoryNames = new ArrayList();
+        categoryNames.add("Airport");
+
         // RecyclerView code
         RecyclerView directionsRC = findViewById(R.id.directionsCategoryRecyclerView);
-
+        DirectionsCategoryAdapter adapter = new DirectionsCategoryAdapter(this, categoryNames);
+        directionsRC.setAdapter(adapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        directionsRC.setLayoutManager(llm);
     }
 }
---
+++ app/src/main/java/com/example/yultravel/DirectionsCategoryAdapter.java
@@ -0,0 +1,67 @@
+package com.example.yultravel;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DirectionsCategoryAdapter extends RecyclerView.Adapter<DirectionsCategoryAdapter.DirectionsCategoryViewHolder> {
+    private ArrayList<String> category_names;
+    private LayoutInflater mInflater;
+
+    public static class DirectionsCategoryViewHolder extends RecyclerView.ViewHolder {
+        final TextView categoryTxt;
+        public DirectionsCategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryTxt = itemView.findViewById(R.id.categoryTextView);
+        }
+    }
+
+    /**
+     * Sets variables of adapter
+     * @param ctx
+     * @param category_names
+     */
+    public DirectionsCategoryAdapter(Context ctx, ArrayList<String> category_names) {
+        mInflater = LayoutInflater.from(ctx);
+        this.category_names = category_names;
+    }
+
+    /**
+     * Creates a viewholder
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public DirectionsCategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.directions_category, parent, false);
+        return new DirectionsCategoryAdapter.DirectionsCategoryViewHolder(mItemView);
+    }
+
+    /**
+     *
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull DirectionsCategoryViewHolder holder, int position) {
+        holder.categoryTxt.setText(category_names.get(position));
+    }
+
+    /**
+     * Returns the size of the list
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return category_names.size();
+    }
+}
