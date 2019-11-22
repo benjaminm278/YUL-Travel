57e5b834d8a306e2fc299a19dcb535d4ff953a3b
Benjamin Mah <mbenjamin0279@gmail.com>
2019-11-22 15:33:27 +0100

    Revert "Merge branch 'master' into 'settingUpdate'"
    This reverts commit 8d2f41e4b7c420260c434f726ba5049127dd253e

--- app/src/main/java/com/example/yultravel/Spots/Spot.java
+++ app/src/main/java/com/example/yultravel/Spots/Spot.java
@@ -3,31 +3,25 @@ package com.example.yultravel.Spots;
 public class Spot {
     private String title;
     private String description;
-    private String url;
-    private String address;
 
     /**
      *
      * @param spotName
      */
-    public Spot(String spotName, String description, String url, String address) {
+    public Spot(String spotName, String description) {
         this.title = spotName;
         this.description = description;
-        this.url = url;
-        this.address = address;
     }
 
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getTitle() {
         return this.title;
-    }
-
-    public String getURL() { return this.url; }
-
-    public String getAddress() {
-        return this.address;
     }
 }
