cce26bcb21ca75f47914b6a6f1e1a2968b317218
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2019-11-22 15:21:47 -0500

    Contain bug fixes

--- app/src/main/java/com/example/yultravel/Directions/MoreDirectionsActivity.java
+++ app/src/main/java/com/example/yultravel/Directions/MoreDirectionsActivity.java
@@ -2,6 +2,7 @@ package com.example.yultravel.Directions;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,11 +27,44 @@ public class MoreDirectionsActivity extends AppCompatActivity {
         t.setText(category);
 
         ArrayList<Location> locations = new ArrayList<>();
-        locations.add(new Location("Decarie Hot Dogs", "953 Boulevard Decarie"));
-        locations.add(new Location("McDonalds", "910 Cote-Vertu"));
-        locations.add(new Location("Mikes", "7101 Cote-Vertu"));
-        locations.add(new Location("Abid's hot spicy chicken", "999 Volcano Land"));
-        locations.add(new Location("Sliced Dice", "777 Casino"));
+
+        if (category.compareToIgnoreCase("airport") == 1) {
+            locations.add(new Location("Montreal International Airport", "YUL Airport"));
+        }
+        else if (category.compareToIgnoreCase("metro (subway)") == 1) {
+            locations.add(new Location("Find nearest metro station", "Nearest metro station"));
+            locations.add(new Location("Orange line station", "Nearest orange line metro station"));
+            locations.add(new Location("Yellow line station", "Nearest yellow line metro station"));
+            locations.add(new Location("Green line station", "Nearest green line metro station"));
+            locations.add(new Location("Blue line station", "Nearest blue line metro station"));
+        }
+        else if (category.compareToIgnoreCase("parks") == 1) {
+            locations.add(new Location("Find nearest metro station", "Nearest metro station"));
+            locations.add(new Location("Decarie Hot Dogs", "953 Boulevard Decarie"));
+        }
+        else if (category.compareToIgnoreCase("restaurants") == 1) {
+            locations.add(new Location("Find nearest metro station", "Nearest metro station"));
+            locations.add(new Location("Decarie Hot Dogs", "953 Boulevard Decarie"));
+            locations.add(new Location("McDonalds", "910 Cote-Vertu"));
+            locations.add(new Location("Mikes", "7101 Cote-Vertu"));
+            locations.add(new Location("Abid's hot spicy chicken", "999 Volcano Land"));
+            locations.add(new Location("Sliced Dice", "777 Casino"));
+        }
+        else if (category.compareToIgnoreCase("shops") == 1) {
+            locations.add(new Location("Find nearest shop", "Nearest metro station"));
+            locations.add(new Location("Decarie Hot Dogs", "953 Boulevard Decarie"));
+            locations.add(new Location("McDonalds", "910 Cote-Vertu"));
+            locations.add(new Location("Mikes", "7101 Cote-Vertu"));
+            locations.add(new Location("Abid's hot spicy chicken", "999 Volcano Land"));
+            locations.add(new Location("Sliced Dice", "777 Casino"));
+        }
+        else if (category.compareToIgnoreCase("stadium") == 1) {
+            locations.add(new Location("Find nearest metro station", "Nearest metro station"));
+            locations.add(new Location("Olympic stadium", "910 Cote-Vertu"));
+            locations.add(new Location("Mikes", "7101 Cote-Vertu"));
+            locations.add(new Location("Abid's hot spicy chicken", "999 Volcano Land"));
+            locations.add(new Location("Sliced Dice", "777 Casino"));
+        }
 
         RecyclerView v = findViewById(R.id.categoryItemsRecyclerView);
         MoreDirectionsCategoryAdapter adapter =
