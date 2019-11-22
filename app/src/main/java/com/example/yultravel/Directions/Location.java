f8ac6070db9256131aa74a2e02503cf48b2891e7
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2019-11-22 11:50:52 -0500

    Create Location class

---
+++ app/src/main/java/com/example/yultravel/Directions/Location.java
@@ -0,0 +1,4 @@
+package com.example.yultravel.Directions;
+
+public class Location {
+}
--- app/src/main/java/com/example/yultravel/Directions/MoreDirectionsCategoryAdapter.java
+++ app/src/main/java/com/example/yultravel/Directions/MoreDirectionsCategoryAdapter.java
@@ -1,24 +1,38 @@
 package com.example.yultravel.Directions;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.yultravel.R;
+
+import java.util.ArrayList;
+
 public class MoreDirectionsCategoryAdapter extends
-        RecyclerView.Adapter<MoreDirectionsCategoryAdapter.MoreDirectionsCategoryViewHolder>{
+        RecyclerView.Adapter<MoreDirectionsCategoryAdapter.MoreDirectionsCategoryViewHolder> {
+    private LayoutInflater mInflater;
+    private Context ctx;
+
     public class MoreDirectionsCategoryViewHolder extends RecyclerView.ViewHolder {
         public MoreDirectionsCategoryViewHolder(@NonNull View itemView) {
             super(itemView);
         }
     }
 
+    public MoreDirectionsCategoryAdapter(Context ctx, ArrayList<Location> a) {
+        this.ctx = ctx;
+    }
+
     @NonNull
     @Override
     public MoreDirectionsCategoryAdapter.MoreDirectionsCategoryViewHolder onCreateViewHolder
             (@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mItemView = mInflater.inflate(R.layout.card_item_location, parent, false);
+        return new MoreDirectionsCategoryAdapter.MoreDirectionsCategoryViewHolder(mItemView);
     }
 
     @Override
