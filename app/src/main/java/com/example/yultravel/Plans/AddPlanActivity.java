25ea9ff24cd8a9a53ab4e7c186050ff189d36151
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2019-11-30 12:39:44 -0500

    Add spinner for new plan form

--- app/src/main/java/com/example/yultravel/Plans/AddPlanActivity.java
+++ app/src/main/java/com/example/yultravel/Plans/AddPlanActivity.java
@@ -3,13 +3,41 @@ package com.example.yultravel.Plans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.yultravel.R;
 
-public class AddPlanActivity extends AppCompatActivity {
+public class AddPlanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plan);
+
+        // Spinner code
+        Spinner s = (Spinner) findViewById(R.id.activityTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activity_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        s.setAdapter(adapter);
+
+        if (s != null) {
+            s.setOnItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String choice = parent.getItemAtPosition(position).toString();
+        Log.d("bangbang", choice);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
