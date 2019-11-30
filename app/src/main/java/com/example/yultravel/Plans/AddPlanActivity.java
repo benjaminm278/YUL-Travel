000777f846828f438998e4db4528181db55fe72e
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2019-11-30 13:34:01 -0500

    Implement date picker fragment

--- app/src/main/java/com/example/yultravel/Plans/AddPlanActivity.java
+++ app/src/main/java/com/example/yultravel/Plans/AddPlanActivity.java
@@ -1,18 +1,32 @@
 package com.example.yultravel.Plans;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.yultravel.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddPlanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String currentActivityType;
+    private String date;
+    private TextView dateTxtView;
+    private TextView timeTxtView;
+    public final String DATE_PICKER_TAG = "datePicker";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +42,71 @@ public class AddPlanActivity extends AppCompatActivity implements AdapterView.On
         if (s != null) {
             s.setOnItemSelectedListener(this);
         }
+
+        dateTxtView = findViewById(R.id.dateTextView);
+        timeTxtView = findViewById(R.id.timeTextView);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String choice = parent.getItemAtPosition(position).toString();
-        Log.d("bangbang", choice);
+        currentActivityType = parent.getItemAtPosition(position).toString();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    /**
+     * Adds plan to database after verification
+     * @param view
+     */
+    public void addPlan(View view) {
+        // Get plan details
+        EditText e = findViewById(R.id.nameEditText);
+        String name = e.getText().toString();
+        EditText locationEditTxt = findViewById(R.id.locationEditText);
+        String location = locationEditTxt.getText().toString();
+
+        // Verify if plan details are valid
+
+        // If so, create plan object,
+
+        // Else don't add plan
+    }
+
+    public void openDateFragment(View view) {
+        DialogFragment d = new DatePickerFragment();
+        d.show(getSupportFragmentManager(), DATE_PICKER_TAG);
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        // Converts integers to strings
+        String month_str = Integer.toString(month+1); // Internally month starts at 0
+        String day_str = Integer.toString(day);
+        String year_str = Integer.toString(year);
+
+        // Formats message to be displayed
+        String date_msg = day_str + "/" + month_str + "/" + year_str;
+
+        // Displays message
+        Log.d("bangbang", date_msg);
+
+        // Add date to object
+        SimpleDateFormat currentFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dateObj;
+        String newDateFormat = "";
+
+        try {
+            dateObj = currentFormat.parse(date_msg);
+            SimpleDateFormat newFormat = new SimpleDateFormat("MMMM dd, yyyy");
+            newDateFormat = newFormat.format(dateObj);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        date = newDateFormat;
+        dateTxtView.setText(date);
     }
 }
